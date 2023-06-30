aa51c3e1a76afb85cb0bb9754261eef4fb780d31
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-06-30 13:26:53 -0500

    se crea repositorio

---
+++ src/test/java/com/springboot/app/msconfigserver/SpringbootMsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
